bdf6a2524e184f009f48f312c5b513a40aa25ab3
Christian <christianx111.ls@gmail.com>
2026-08-13 17:07:12 -0300

    repository created

--- ms-email/src/main/java/com/techchristian/ms/email/database/repository/EmailRepository.java
+++ ms-email/src/main/java/com/techchristian/ms/email/database/repository/EmailRepository.java
@@ -1,4 +1,9 @@
 package com.techchristian.ms.email.database.repository;
 
-public interface EmailRepository {
+import com.techchristian.ms.email.database.models.EmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID > {
 }
