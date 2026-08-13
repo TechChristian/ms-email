8d920cf5e01da2aa7ddc5988fb1cdaef4e45afbc
Christian <christianx111.ls@gmail.com>
2026-08-13 16:28:22 -0300

    feat: create model of email and their informations

--- ms-email/src/main/java/com/techchristian/ms/email/database/models/EmailModel.java
+++ ms-email/src/main/java/com/techchristian/ms/email/database/models/EmailModel.java
@@ -1,4 +1,31 @@
 package com.techchristian.ms.email.database.models;
 
+import com.techchristian.ms.email.database.enums.StatusEmail;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_EMAILS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class EmailModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID emailId;
+    private UUID userId;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sentDateEmail;
+    private StatusEmail statusEmail;
 }
