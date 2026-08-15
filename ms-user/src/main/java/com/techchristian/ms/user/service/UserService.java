4ef01b178cd1847973046b422dce559578ed9ac1
Christian <christianx111.ls@gmail.com>
2026-08-15 00:14:49 -0300

    feat: add method of add new user in application

--- ms-user/src/main/java/com/techchristian/ms/user/service/UserService.java
+++ ms-user/src/main/java/com/techchristian/ms/user/service/UserService.java
@@ -1,4 +1,21 @@
 package com.techchristian.ms.user.service;
 
+import com.techchristian.ms.user.database.model.UserModel;
+import com.techchristian.ms.user.database.repository.UserRepository;
+import com.techchristian.ms.user.dto.UserCreateDto;
+import com.techchristian.ms.user.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserModel addUser(UserCreateDto dto) {
+        UserModel userRequestToEntity = UserMapper.toEntity(dto);
+        return userRepository.save(userRequestToEntity);
+    }
 }
