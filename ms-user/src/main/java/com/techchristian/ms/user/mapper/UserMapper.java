ae034cb38ede6f6e8a9d4c5bbbea8b7bcbe04c97
Christian <christianx111.ls@gmail.com>
2026-08-15 00:14:08 -0300

    feat: created user mapper for to entity and to response

--- ms-user/src/main/java/com/techchristian/ms/user/mapper/UserMapper.java
+++ ms-user/src/main/java/com/techchristian/ms/user/mapper/UserMapper.java
@@ -1,4 +1,23 @@
 package com.techchristian.ms.user.mapper;
 
+import com.techchristian.ms.user.database.model.UserModel;
+import com.techchristian.ms.user.dto.UserCreateDto;
+import com.techchristian.ms.user.dto.UserResponseDto;
+
 public class UserMapper {
+    public static UserModel toEntity(UserCreateDto dto){
+        UserModel userModel = new UserModel();
+        return userModel.builder()
+                .email(dto.email())
+                .name(dto.name())
+                .build();
+    }
+
+    public static UserResponseDto toResponseDto(UserModel userModel){
+        return new UserResponseDto(
+                userModel.getUserId(),
+                userModel.getEmail(),
+                userModel.getName()
+        );
+    }
 }
