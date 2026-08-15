32928f04372aeced53c1a86597d44c1f8fc1bae4
Christian <christianx111.ls@gmail.com>
2026-08-15 00:15:30 -0300

    feat: created user controller and PostMapping to add new user

--- ms-user/src/main/java/com/techchristian/ms/user/controller/UserController.java
+++ ms-user/src/main/java/com/techchristian/ms/user/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.techchristian.ms.user.controller;
 
+import com.techchristian.ms.user.database.model.UserModel;
+import com.techchristian.ms.user.dto.UserCreateDto;
+import com.techchristian.ms.user.dto.UserResponseDto;
+import com.techchristian.ms.user.mapper.UserMapper;
+import com.techchristian.ms.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/api/users")
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> createUser (@Valid @RequestBody UserCreateDto dto){
+        UserModel user = userService.addUser(dto);
+
+        UserResponseDto response = UserMapper.toResponseDto(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
