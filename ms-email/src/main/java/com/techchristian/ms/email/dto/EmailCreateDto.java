57df927f8fedd51460db7ac7479acf87c362aa92
Christian <christianx111.ls@gmail.com>
2026-08-14 23:35:49 -0300

    feat: add validations to email create dto

--- ms-email/src/main/java/com/techchristian/ms/email/dto/EmailCreateDto.java
+++ ms-email/src/main/java/com/techchristian/ms/email/dto/EmailCreateDto.java
@@ -1,11 +1,22 @@
 package com.techchristian.ms.email.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
 public record EmailCreateDto (
+        @NotNull
         UUID userId,
+        @Email(message = "invalid email from.")
+        @NotBlank(message = "email from is required.")
         String emailFrom,
+        @Email(message = "invalid email to")
+        @NotBlank(message = "email to is required.")
         String emailTo,
+        @NotBlank(message = "subject is required.")
         String subject,
+        @NotBlank(message = "text is required")
         String text
 ){ }
