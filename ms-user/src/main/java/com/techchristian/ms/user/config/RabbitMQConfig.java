77ddfefe3b9c05898dadefd5e87db796f3fdf12e
Christian <christianx111.ls@gmail.com>
2026-08-16 22:55:58 -0300

    feat: add new method of user producer

---
+++ ms-user/src/main/java/com/techchristian/ms/user/config/RabbitMQConfig.java
@@ -0,0 +1,4 @@
+package com.techchristian.ms.user.config;
+
+public class RabbitMQConfig {
+}
--- ms-user/src/main/java/com/techchristian/ms/user/service/UserService.java
+++ ms-user/src/main/java/com/techchristian/ms/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.techchristian.ms.user.database.model.UserModel;
 import com.techchristian.ms.user.database.repository.UserRepository;
 import com.techchristian.ms.user.dto.UserCreateDto;
 import com.techchristian.ms.user.mapper.UserMapper;
+import com.techchristian.ms.user.producer.UserProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,10 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final UserProducer userProducer;
 
     @Transactional
     public UserModel addUser(UserCreateDto dto) {
         UserModel userRequestToEntity = UserMapper.toEntity(dto);
+        userProducer.sendWelcomeEmail(userRequestToEntity);
         return userRepository.save(userRequestToEntity);
     }
 }
