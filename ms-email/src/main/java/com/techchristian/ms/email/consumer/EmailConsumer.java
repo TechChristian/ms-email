8e542d1c681292e5bff3cc1bd6fbffb1a4dcd312
Christian <christianx111.ls@gmail.com>
2026-08-16 22:05:46 -0300

    feat: add method sendEmail

--- ms-email/src/main/java/com/techchristian/ms/email/consumer/EmailConsumer.java
+++ ms-email/src/main/java/com/techchristian/ms/email/consumer/EmailConsumer.java
@@ -1,4 +1,23 @@
 package com.techchristian.ms.email.consumer;
 
+import com.techchristian.ms.email.database.models.EmailModel;
+import com.techchristian.ms.email.dto.EmailCreateDto;
+import com.techchristian.ms.email.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+
+@RequiredArgsConstructor
 public class EmailConsumer {
+
+    private final EmailService emailService;
+
+    @RabbitListener(queues = "${broker.queue.email.welcome}")
+    public void receiveEmailWelcome(EmailCreateDto emailCreateDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailCreateDto, emailModel);
+
+        // * sendEmail
+        emailService.sendEmail(emailModel);
+    }
 }
