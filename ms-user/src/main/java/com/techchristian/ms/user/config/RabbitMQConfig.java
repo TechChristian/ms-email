10163eb39b442ec9af9ffb35a4e4a92ac9676d82
Christian <christianx111.ls@gmail.com>
2026-08-16 22:57:52 -0300

    feat: created rabbitmqconfig in user for convert objects

--- ms-user/src/main/java/com/techchristian/ms/user/config/RabbitMQConfig.java
+++ ms-user/src/main/java/com/techchristian/ms/user/config/RabbitMQConfig.java
@@ -1,4 +1,14 @@
 package com.techchristian.ms.user.config;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RabbitMQConfig {
+    // * convert messages (object) in Json
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
