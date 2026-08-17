0ec55db7923c5fd8fa2bcd74947b1e0add727304
Christian <christianx111.ls@gmail.com>
2026-08-16 21:36:24 -0300

    feat: created user producer for queue of submit emails

--- ms-user/src/main/java/com/techchristian/ms/user/producer/UserProducer.java
+++ ms-user/src/main/java/com/techchristian/ms/user/producer/UserProducer.java
@@ -1,4 +1,28 @@
 package com.techchristian.ms.user.producer;
 
+import com.techchristian.ms.user.database.model.UserModel;
+import com.techchristian.ms.user.dto.EmailCreateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class UserProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value(value = "${broker.queue.email.welcome}")
+    private String emailWelcome;
+
+    public void sendWelcomeEmail(UserModel userModel){
+        EmailCreateDto emailCreate = new EmailCreateDto();
+
+        emailCreate.setUserId(userModel.getUserId());
+        emailCreate.setEmailTo(userModel.getEmail());
+        emailCreate.setSubject("Registration successful");
+        emailCreate.setText(userModel.getName() + ", Welcome to the user!");
+
+        rabbitTemplate.convertAndSend(emailWelcome, emailCreate);
+    }
 }
