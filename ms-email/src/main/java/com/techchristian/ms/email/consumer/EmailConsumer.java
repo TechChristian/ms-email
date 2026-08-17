0bde46540a713d3fa1709cb37162aa6d42c71ca6
Christian <christianx111.ls@gmail.com>
2026-08-16 22:58:51 -0300

    feat: add annotation payload for request email in queue

--- ms-email/src/main/java/com/techchristian/ms/email/consumer/EmailConsumer.java
+++ ms-email/src/main/java/com/techchristian/ms/email/consumer/EmailConsumer.java
@@ -6,17 +6,19 @@ import com.techchristian.ms.email.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 public class EmailConsumer {
 
     private final EmailService emailService;
 
     @RabbitListener(queues = "${broker.queue.email.welcome}")
-    public void receiveEmailWelcome(EmailCreateDto emailCreateDto) {
+    public void receiveEmailWelcome(@Payload EmailCreateDto emailCreateDto) {
         EmailModel emailModel = new EmailModel();
         BeanUtils.copyProperties(emailCreateDto, emailModel);
-
         // * sendEmail
         emailService.sendEmail(emailModel);
     }
